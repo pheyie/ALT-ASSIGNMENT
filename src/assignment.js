4372e4626c49b9d6228d960240cd9a40c8e16e50
olusoga olumuyiwa <33479614+pheyie@users.noreply.github.com>
2022-07-11 19:08:49 +0000

    attempting challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+
+  for(i=0;i<arrayOfNumbers.length;i++){
+      sum += arrayOfNumbers[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
